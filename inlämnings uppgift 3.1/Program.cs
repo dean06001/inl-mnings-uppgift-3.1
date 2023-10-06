d67cfbccf5550e248a1ccc66a963bef85f7a664f
dean06001 <dean06001@E-PW0234KC>
2023-10-06 12:34:23 +0200

    Add project files.

---
+++ inlämnings uppgift 3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace inlämningsuppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej hur gammal är du?");
+            int boose = int.Parse(Console.ReadLine());
+            if (boose >= 16 && boose <=19)
+            {
+                Console.WriteLine("Grattis du får vara med i tävlingen");
+            }
+            else if (boose > 19)
+            {
+                Console.WriteLine("du är för gammal");
+            }
+            else
+            {
+                Console.WriteLine("du är för ung");
+            }
+            Console.ReadKey();
+        }
+    }
+}
